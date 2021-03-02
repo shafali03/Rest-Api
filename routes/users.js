5ccdcf4f39fda39de6ff24feebd5df3eadb4f8c7
shafali03 <shaf03sm@gmail.com>
2021-03-02 20:56:14 +0000

    Created a get router to find id of users

--- routes/users.js
+++ routes/users.js
@@ -20,5 +20,14 @@ router.post('/', (req, res) => {
   res.send(`User with the name ${user.firstName} added to the database`)
 })
 
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+
+  const foundUser = users.find((user) => user.id === id)
+
+  res.send(foundUser)
+})
+
 export default router
 

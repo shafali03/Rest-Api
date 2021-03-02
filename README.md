5c7b60311054847d363aaea87f94b3e7bf808d37
shafali03 <shaf03sm@gmail.com>
2021-03-02 16:09:55 +0000

    Installed uuid and pushing user with a unique id

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Npm packages installed
 npm init -y  
 npm install --save express
 npm install --save-dev nodemon   
-
+npm install uuid
 ```
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 const router = express.Router()
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 20
-  }
-]
+const users = []
 
 
 // all routes here start with /users
@@ -25,7 +15,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const user = req.body
 
-  users.push(user)
+  users.push({ ...user, id: uuidv4() })
 
   res.send(`User with the name ${user.firstName} added to the database`)
 })
